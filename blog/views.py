c06d6587afb3e9f370d5dde3cbb128aa537c5b8f
qcmiao <46510571@qq.com>
2018-06-28 15:52:44 +0800

    changed the html for site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
 # Create your views here.
+def homePage(request):
+    return HttpResponse("<h1>This is home page<h1>")
+def post_list(request):
+    return render(request, 'blog/post_list.html',{})
