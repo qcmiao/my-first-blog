90f868233161d71246cbee08db929864874504b2
qcmiao <46510571@qq.com>
2018-06-29 10:30:25 +0800

    post_updates

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 def homePage(request):
     return HttpResponse("<h1>This is home page<h1>")
 def post_list(request):
-    return render(request, 'blog/post_list.html',{})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html',{'posts': posts})
